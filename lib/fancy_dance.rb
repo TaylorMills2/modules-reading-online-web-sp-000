ff37e3c7f0401e6f21284c1f840a130b3695fb33
Taylor Mills <taylormills24@yahoo.com>
2020-02-04 10:58:12 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -4,4 +4,8 @@ module FancyDance
     def twirl "I'm twirling!"
     end
     
-    def jump "Look how high "
+    def jump "Look how high I'm jumping!"
+    end
+    
+    def pirouette "I'm doing a pirouette"
+    end
