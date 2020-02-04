288895524a9ef3a9010d84a1a70955578e31215a
Taylor Mills <taylormills24@yahoo.com>
2020-02-04 11:08:34 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,18 +1,4 @@
 class Dancer
-  include Dance
-  
-  attr_accessor :name
-  
-  def initialize (name)
-    @name = name
-  end
-end
-
-class Dancer
-  extend MetaDancing
-end
-
-class Dancer
   extend FancyDance::ClassMethods 
   include FancyDance::InstanceMethods
 end
